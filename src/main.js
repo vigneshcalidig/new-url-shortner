88a5e964c90e600e9dc1cca4ac9d17ab3aff50ca
vigneshcalidig <vignesh@calidig.com>
2023-11-15 16:55:23 +0530

    added log

--- src/main.js
+++ src/main.js
@@ -21,6 +21,8 @@ export default async ({ res, req, log, error }) => {
       return res.send({ ok: false, error: err.message }, 400);
     }
 
+    console.log(req.body.url);
+
     const urlEntry = await appwrite.createURLEntry(
       req.body.url,
       req.body.shortCode ?? generateShortCode(),
